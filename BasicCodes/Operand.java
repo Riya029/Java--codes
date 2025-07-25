b88e891fd4058875fcfa8a6e6bdc0fef4f052903
Riya <riya198shrivastava@gmail.com>
2025-07-25 23:45:32 +0530

    Add files via upload

---
+++ BasicCodes/Operand.java
@@ -0,0 +1,15 @@
+public class Operand{
+    public static void main(String args[]){
+        int a = 7;
+        int b = 4;
+        int c = a / b;// it will give the integer value after the division
+        double d = a % b; // it will give the remainder of a and b ie 1.0 when float and when integer it will give 1 
+        System.out.println(a -b);
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(c);
+        System.out.println(d);
+        // System.out.println(a and b);
+    
+    }
+}
