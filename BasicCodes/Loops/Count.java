767837760a8fa57e499f45acf64c0af35c160431
Riya <riya198shrivastava@gmail.com>
2025-07-29 19:51:21 +0530

    Add files via upload

---
+++ BasicCodes/Loops/Count.java
@@ -0,0 +1,18 @@
+// count the number of digits of a positive number
+
+import java.util.Scanner;
+public class Count {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n = in.nextInt();
+        
+        //
+        int count = 0;
+        while( n > 0 ){
+             n = n / 10;
+            count = count + 1;
+        }
+        System.out.println(count);
+    }
+}
