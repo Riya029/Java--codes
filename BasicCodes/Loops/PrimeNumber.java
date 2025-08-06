b5b118e8e60e406a3adde0d1bbdac43945a59e29
Riya <riya198shrivastava@gmail.com>
2025-08-06 13:55:17 +0530

    Add files via upload

--- BasicCodes/Loops/PrimeNumber.java
+++ BasicCodes/Loops/PrimeNumber.java
@@ -1,27 +1,25 @@
-// find number is prime or not
+// find prime factors of the number entered by the user eg: 60 = 2*2*3*5
 import java.util.Scanner;
-public class PrimeNumber {
-    /**
-     * @param args
-     */
+public class PrimeNumber{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number:");
-        int n = in.nextInt();
+        long n = in.nextInt();
 
-        // check
-        if (n <= 1){
-            System.out.println("not prime");
-        }
-        else{
-            int i = 2;
-            while(i < n ){
-                if(n % i == 0){
-                    System.out.println(n + "not prime number");
-                }
-                i = i + 1;
+        // find
+        System.out.println("The prime factors are:");
+        int i = 2;
+        while(n > 1){
+            if(n % i == 0){
+                 System.out.println(i);
+                n = n / i;  
             }
-            System.out.println(n + " prime number");
+            else{
+                i = i + 1; 
+            }
+           
+
         }
+        in.close();
     }
 }
