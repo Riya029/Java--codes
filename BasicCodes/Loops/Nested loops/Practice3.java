ade3b396f18cb1b3478b875d8c5e56ec57e5e732
Riya <riya198shrivastava@gmail.com>
2025-09-26 23:49:59 +0530

    Add files via upload

---
+++ BasicCodes/Loops/Nested loops/Practice3.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Scanner;
+public class Practice3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("enter the value of n: ");
+        int n = in.nextInt();
+
+        // loop apply 
+        
+    }    
+}
